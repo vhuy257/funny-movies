458e7d7f0cbf51db072b999f26573f23f5ef5c1b
huy <huy.nguyen@monimedia.com>
2019-10-24 17:07:50 +0700

    fix

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,6 +5,13 @@ import shippedJson from '../animated-icons/shipped/shipped.json';
 import openJson from '../animated-icons/open-sign/open-sign.json';
 import shieldJson from '../animated-icons/shield/shield.json';
 import refreshJson from '../animated-icons/refresh-wait/refresh-wait.json';
+import Masonry from 'react-masonry-component';
+
+const masonryOptions = {
+    transitionDuration: 0
+};
+
+const imagesLoadedOptions = { background: '.my-bg-image-el' }
 
 const shipping = {
     autoplay: false,
@@ -46,20 +53,47 @@ class HomePage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            isStoped: false,
+            isStoped1: false,
+            isStoped2: false,
+            isStoped3: false,
+            isStoped4: false,
         }
     }
 
-    play() {
-        this.setState({
-            isStoped: false,
-        })
+    play(isStoped) {
+        switch(isStoped) {
+            case '1':
+                this.setState({ isStoped1: false }) 
+            break;
+            case '2':
+                this.setState({ isStoped2: false }) 
+            break;
+            case '3':
+                this.setState({ isStoped3: false })
+            break;
+            case '4':
+                this.setState({isStoped4: false})
+            break;
+            default:
+        }
     }
 
-    stop() {
-        this.setState({
-            isStoped: true,
-        });
+    stop(isStoped) {
+        switch(isStoped) {
+            case '1':
+                this.setState({ isStoped1: true }) 
+            break;
+            case '2':
+                this.setState({ isStoped2: true }) 
+            break;
+            case '3':
+                this.setState({ isStoped3: true })
+            break;
+            case '4':
+                this.setState({isStoped4: true})
+            break;
+            default:
+        }
     }
 
     render() {
@@ -68,50 +102,98 @@ class HomePage extends React.Component {
                 <SliderLayout/>
                 <div className="container mx-auto my-2">
                         <div className="flex items-center justify-around my-8">
-                            <div className="" onMouseOver={() => {this.play()}} onMouseOut={() => {this.stop()}}>
+                            <div className="" onMouseOver={() => {this.play('1')}} onMouseOut={() => {this.stop('1')}}>
                             <Lottie 
                                 options={
                                     shipping
                                 }
-                                isStopped={this.state.isStoped}
+                                isStopped={this.state.isStoped1}
                                 height={40}
                                 width={40}
                             />
                                 Free shipping
                             </div>
-                            <div className="" onMouseOver={() => {this.play()}} onMouseOut={() => {this.stop()}}>
+                            <div className="" onMouseOver={() => {this.play('2')}} onMouseOut={() => {this.stop('2')}}>
                             <Lottie 
                                 options={
                                     support
                                 }
-                                isStopped={this.state.isStoped}
+                                isStopped={this.state.isStoped2}
                                 height={40}
                                 width={40}
                             />
                                 Buyer Support 24/7
                             </div>
-                            <div className="" onMouseOver={() => {this.play()}} onMouseOut={() => {this.stop()}}>
+                            <div className="" onMouseOver={() => {this.play('3')}} onMouseOut={() => {this.stop('3')}}>
                             <Lottie 
                                 options={
                                     shield
                                 }
-                                isStopped={this.state.isStoped}
+                                isStopped={this.state.isStoped3}
                                 height={40}
                                 width={40}
                             />
                                 Total security
                             </div>
-                            <div className="" onMouseOver={() => {this.play()}} onMouseOut={() => {this.stop()}}>
+                            <div className="" onMouseOver={() => {this.play('4')}} onMouseOut={() => {this.stop('4')}}>
                             <Lottie 
                                 options={
                                     refresh
                                 }
-                                isStopped={this.state.isStoped}
+                                isStopped={this.state.isStoped4}
                                 height={40}
                                 width={40}
                             />
                                 Return policy
                             </div>
+                        </div>
+                        
+                        <Masonry
+                            className={'my-gallery-class flex'} // default ''
+                            elementType={'ul'} // default 'div'
+                            options={masonryOptions} // default {}
+                            disableImagesLoaded={false} // default false
+                            updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
+                            imagesLoadedOptions={imagesLoadedOptions} // default {}
+                        >    
+                            <li className="image-element-class">
+                                <div className="inner-img">
+                                    <img src="./images/photo-5.jpg" alt=""/>
+                                </div>
+                            </li>
+                            <li className="image-element-class">      
+                                <div className="inner-img">
+                                    <img src="./images/photo-3.jpg" alt=""/>
+                                </div>                          
+                            </li>
+                            <li className="image-element-class">
+                                <div className="inner-img">
+                                    <img src="./images/photo-2.jpg" alt=""/>
+                                </div>
+                            </li>
+                            <li className="image-element-class">
+                                <div className="inner-img">
+                                    <img src="./images/photo-1.jpg" alt=""/>
+                                </div>
+                            </li>
+                            <li className="image-element-class">
+                                <div className="inner-img">
+                                    <img src="./images/photo-4.jpg" alt=""/>
+                                </div>
+                            </li>                                                            
+                        </Masonry>
+                        
+                        <div className="special-wrapper container flex">
+                                <div className="text-center w-full my-10">
+                                    <h2 className="text-lg uppercase font-bold text-teal-600">Special</h2>
+                                </div>
+                                <ul>
+                                    <li>
+                                        <div className="product-item">
+
+                                        </div>
+                                    </li>
+                                </ul>
                         </div>
                 </div>
             </>
