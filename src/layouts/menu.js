012b587e3781042468ab008e85f309750d0f5fd4
huy <huy.nguyen@monimedia.com>
2019-12-11 17:38:28 +0700

    fixed menu list

--- src/layouts/menu.js
+++ src/layouts/menu.js
@@ -13,22 +13,28 @@ const Menu = () => {
                                 <span>{item.title}</span>
                             </Link>
                             {
-                                item.childItems &&
-                                item.childItems.map(item => (
+                                item.childItems && 
+                                (
                                     <div className="dropdown-menu-child shadow-md">
                                         <div className="container flex flex-wrap p-2">
-                                            <ul className="w-1/2 m-5">
-                                                <li className="font-bold">{item.title}</li>
-                                                {item.listItems.map(item => (
-                                                    <li>{item.name}</li>
-                                                ))}
-                                            </ul>
-                                            <div className="image-menu w-1/5">
-                                                <img src="./images/menu-marketing-image.jpg" alt="" className="object-cover w-full h-full"/>
-                                            </div>
+                                        {
+                                            item.childItems.map(item => (
+                                                <>
+                                                    <ul className="w-1/5 m-5">
+                                                        <li className="font-bold">{item.title}</li>
+                                                        {item.listItems.map(item => (
+                                                            <li>{item.name}</li>
+                                                        ))}
+                                                    </ul>
+                                                </>
+                                            ))
+                                        }
+                                        <div className="image-menu w-1/5">
+                                            <img src="./images/menu-marketing-image.jpg" alt="" className="object-cover w-full h-full"/>
+                                        </div>
                                         </div>
                                     </div>
-                                ))
+                                )
                             }
                         </li>              
                     ))
