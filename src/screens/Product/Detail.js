93c8f71b101c449c679c6a776c0e37d37dd2be60
huy <huy.nguyen@monimedia.com>
2020-02-05 17:25:46 +0700

    fix

--- src/screens/Product/Detail.js
+++ src/screens/Product/Detail.js
@@ -43,6 +43,17 @@ const Detail = (props) => {
         setQty({value: parseInt(newvalue.target.value)})
     }
 
+    const changeQty = (params) => {
+        if(params === 'plus') {
+            setQty({value: parseInt(qty.value + 1)})
+        } else {
+            if(qty.value === 1) {
+                return false;
+            }
+            setQty({value: parseInt(qty.value - 1)})
+        }   
+    }
+
     return (
         <>
             <div className="breadcrumbs bg-orange-400 text-white">
@@ -94,13 +105,30 @@ const Detail = (props) => {
                         </div>
                         <div className="flex items-center content-center border-t border-gray-200">
                             <div className="quantity">
-                                <button className="minus outline-none"><i className="icon-minus"></i></button>
+                                <button className="minus outline-none" onClick={() => {changeQty('minus')}}><i className="icon-minus"></i></button>
                                 <input type="number" value={qty.value} onChange={(value) => {setQuantity(value)}} className="border px-2 py-1 outline-none w-12 m-4 text-center"/>
-                                <button className="plus outline-none"><i className="icon-plus"></i></button>
+                                <button className="plus outline-none" onClick={() => {changeQty('plus')}}><i className="icon-plus"></i></button>
                             </div>
                             <button className="add-to-cart bg-red-500 text-white p-4 py-2 ml-2 hover:bg-red-600">
                                 Add to cart
                             </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="tab-content">
+                    <div className="tab-header mt-10 border-b border-gray-200">
+                        <ul className="flex">
+                            <li className="p-2 active font-bold text-teal-700">Description</li>
+                            <li className="p-2">Review</li>
+                        </ul>
+                    </div>
+                    <div className="tab-content p-2">
+                        <div className="desciption">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem velit dolores ex cum aperiam, sed quae reiciendis vitae cumque aut distinctio dicta laudantium iusto, dolor ipsa fugit officiis aliquam. Doloremque?
+                        </div>
+                        <div className="review">
+
                         </div>
                     </div>
                 </div>
