430a06368f51d496d75ca1a2b4dae6c68c41664a
unknown <vhuy2571990@gmail.com>
2020-02-09 21:16:03 +0700

    add to cart function

--- src/screens/Product/Detail.js
+++ src/screens/Product/Detail.js
@@ -1,8 +1,11 @@
-import React, {useState} from 'react';
-import { useParams } from 'react-router-dom';
+import React, {Component} from 'react';
+import { withRouter } from 'react-router';
 import ListProducts from '../../data/Products';
 import "react-image-gallery/styles/css/image-gallery.css";
 import ImageGallery from 'react-image-gallery';
+import connect from 'react-redux/es/connect/connect';
+import {bindActionCreators} from 'redux';
+import * as cartActions from '../../store/actions/cart';
 
 const images = [
     {
@@ -26,115 +29,141 @@ const images = [
       thumbnail: 'https://picsum.photos/id/1022/250/150/',
     },
 ];
-   
-const Detail = (props) => {
-    let {id} = useParams();
 
-    let product = ListProducts.filter(item => {
-        return item.id === parseInt(id);
-    });
+class Detail extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            qty: 1,
+            product: []
+        }
+    }
+    
+    componentDidMount() {
+        let id = this.props.match.params.id;
+        this.setState({
+            product: ListProducts.filter(item => {
+                return item.id === parseInt(id);
+            })
+        });
+    }
 
-    const [qty, setQty] = useState({value: 1});
+    addtoCart() {
+        this.props.addProductToCart({
+            product: this.state.product[0]
+        });
+    }
 
-    const setQuantity = (newvalue) => {
+    setQuantity = (newvalue) => {
         if(!parseInt(newvalue.target.value) > 0) {
             return false;
         }
-        setQty({value: parseInt(newvalue.target.value)})
+        this.setState({qty: parseInt(newvalue.target.value)})
     }
 
-    const changeQty = (params) => {
+    changeQty = (params) => {
         if(params === 'plus') {
-            setQty({value: parseInt(qty.value + 1)})
+            this.setState({qty: parseInt(this.state.qty + 1)})
         } else {
-            if(qty.value === 1) {
+            if(this.state.qty === 1) {
                 return false;
             }
-            setQty({value: parseInt(qty.value - 1)})
+            this.setState({qty: parseInt(this.state.qty - 1)})
         }   
     }
 
-    return (
-        <>
-            <div className="breadcrumbs bg-orange-400 text-white">
-                <div className="container p-3">
-                    <div className="text-center">
-                        <span className="item">
-                            Home
-                        </span>
-                        <span>
-                            {product[0].name}
-                        </span>
+    render() {
+        return (
+            <>
+                <div className="breadcrumbs bg-orange-400 text-white">
+                    <div className="container p-3">
+                        <div className="text-center">
+                            <span className="item">
+                                Home
+                            </span>
+                            <span>
+                                {this.state.product[0] && this.state.product[0].name}
+                            </span>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="container mt-5">   
-                <div className="flex -mx-4">
-                    <div className="w-3/6 px-4">
-                        <ImageGallery 
-                            autoPlay={true}
-                            showNav={true}
-                            lazyLoad={true}
-                            items={images} 
-                            showPlayButton={false}
-                            showFullscreenButton={false}
-                        />
-                    </div>
-                    <div className="w-3/6 px-4">
-                        <h2 className="text-2xl">
-                            { product[0].name }
-                        </h2>
-                        <h3 className="text-2xl text-orange-600 font-bold">
-                            $ { product[0].price }
-                        </h3>
-                        <div className="list-attribute text-base my-4">
-                            <ul>
-                                <li className="my-1">
-                                    <i className="icon-layers"></i> <span className="ml-3 text-sm font-bold">M</span>
-                                </li>
-                                <li className="my-1">
-                                    <i className="icon-pie-chart"></i> <span className="ml-3 text-sm font-bold">Blue</span>
-                                </li>
-                                <li className="my-1">
-                                    <i className="icon-cursor"></i> <span className="ml-3 text-sm font-bold">Free shipping</span>
-                                </li>
-                            </ul>
-                            <p className="mt-2">
-                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat molestias dolorum maiores voluptatibus itaque aspernatur eius non architecto, accusantium maxime iure cum libero placeat tenetur ad sapiente, at voluptatem tempore!
-                            </p>
+                <div className="container mt-5">   
+                    <div className="flex -mx-4">
+                        <div className="w-3/6 px-4">
+                            <ImageGallery 
+                                autoPlay={true}
+                                showNav={true}
+                                lazyLoad={true}
+                                items={images} 
+                                showPlayButton={false}
+                                showFullscreenButton={false}
+                            />
                         </div>
-                        <div className="flex items-center content-center border-t border-gray-200">
-                            <div className="quantity">
-                                <button className="minus outline-none" onClick={() => {changeQty('minus')}}><i className="icon-minus"></i></button>
-                                <input type="number" value={qty.value} onChange={(value) => {setQuantity(value)}} className="border px-2 py-1 outline-none w-12 m-4 text-center"/>
-                                <button className="plus outline-none" onClick={() => {changeQty('plus')}}><i className="icon-plus"></i></button>
+                        <div className="w-3/6 px-4">
+                            <h2 className="text-2xl">
+                                { this.state.product[0] && this.state.product[0].name }
+                            </h2>
+                            <h3 className="text-2xl text-orange-600 font-bold">
+                                $ { this.state.product[0] && this.state.product[0].price }
+                            </h3>
+                            <div className="list-attribute text-base my-4">
+                                <ul>
+                                    <li className="my-1">
+                                        <i className="icon-layers"></i> <span className="ml-3 text-sm font-bold">M</span>
+                                    </li>
+                                    <li className="my-1">
+                                        <i className="icon-pie-chart"></i> <span className="ml-3 text-sm font-bold">Blue</span>
+                                    </li>
+                                    <li className="my-1">
+                                        <i className="icon-cursor"></i> <span className="ml-3 text-sm font-bold">Free shipping</span>
+                                    </li>
+                                </ul>
+                                <p className="mt-2">
+                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat molestias dolorum maiores voluptatibus itaque aspernatur eius non architecto, accusantium maxime iure cum libero placeat tenetur ad sapiente, at voluptatem tempore!
+                                </p>
                             </div>
-                            <button className="add-to-cart bg-red-500 text-white p-4 py-2 ml-2 hover:bg-red-600">
-                                Add to cart
-                            </button>
+                            <div className="flex items-center content-center border-t border-gray-200">
+                                <div className="quantity">
+                                    <button className="minus outline-none" onClick={() => {this.changeQty('minus')}}><i className="icon-minus"></i></button>
+                                    <input type="number" value={this.state.qty} onChange={(value) => {this.setQuantity(value)}} className="border px-2 py-1 outline-none w-12 m-4 text-center"/>
+                                    <button className="plus outline-none" onClick={() => {this.changeQty('plus')}}><i className="icon-plus"></i></button>
+                                </div>
+                                <button 
+                                onClick={() => {this.addtoCart()}}
+                                className="add-to-cart bg-red-500 text-white p-4 py-2 ml-2 hover:bg-red-600">
+                                    Add to cart
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+    
+                    <div className="tab-content">
+                        <div className="tab-header mt-10 border-b border-gray-200">
+                            <ul className="flex">
+                                <li className="p-2 active font-bold text-teal-700">Description</li>
+                                <li className="p-2">Review</li>
+                            </ul>
+                        </div>
+                        <div className="tab-content p-2">
+                            <div className="desciption">
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem velit dolores ex cum aperiam, sed quae reiciendis vitae cumque aut distinctio dicta laudantium iusto, dolor ipsa fugit officiis aliquam. Doloremque?
+                            </div>
+                            <div className="review">
+    
+                            </div>
                         </div>
                     </div>
                 </div>
-
-                <div className="tab-content">
-                    <div className="tab-header mt-10 border-b border-gray-200">
-                        <ul className="flex">
-                            <li className="p-2 active font-bold text-teal-700">Description</li>
-                            <li className="p-2">Review</li>
-                        </ul>
-                    </div>
-                    <div className="tab-content p-2">
-                        <div className="desciption">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem velit dolores ex cum aperiam, sed quae reiciendis vitae cumque aut distinctio dicta laudantium iusto, dolor ipsa fugit officiis aliquam. Doloremque?
-                        </div>
-                        <div className="review">
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+            </>
+        )
+    }
 }
 
-export default Detail;
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({
+        addProductToCart: cartActions.addProductToCart,
+    }, dispatch)
+}
+
+
+export default connect(null, mapDispatchToProps)(withRouter(Detail));
